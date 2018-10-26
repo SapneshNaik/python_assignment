02e51eab0f185a1a4bbb8f175358688c3d40a834
Sapnesh Naik <sapnesh@kerneldev.com>
2018-10-26 10:20:47 +0530

    updated server code

--- client-server/server.py
+++ client-server/server.py
@@ -1,29 +1,15 @@
-# server.py 
-import socket                                         
-import time
+# echo_server.py
+import socket
 
-# create a socket object
-serversocket = socket.socket(
-	        socket.AF_INET, socket.SOCK_STREAM) 
-
-# get local machine name
-# host = socket.gethostname()         
-
-# print(host)                  
-
-port = 9999                                           
-
-# bind to the port
-serversocket.bind(('127.0.0.1', port))                                  
-
-# queue up to 5 requests
-serversocket.listen(5)                                           
-
+host = ''        # Symbolic name meaning all available interfaces
+port = 12345     # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen(1)
+conn, addr = s.accept()
+print('Connected by', addr)
 while True:
-    # establish a connection
-    clientsocket,addr = serversocket.accept()      
-
-    print("Got a connection from %s" % str(addr))
-    currentTime = time.ctime(time.time()) + "\r\n"
-    clientsocket.send(currentTime.encode('ascii'))
-    clientsocket.close()
+    data = conn.recv(1024)
+    if not data: break
+    conn.sendall(data.decode().upper().encode())
+conn.close()
